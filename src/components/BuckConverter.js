a4b69a3a909f2bbaf2a74bbc7a3918f47be31548
H. Avni Cubukcu <huseyinac@gmail.com>
2022-03-07 08:41:51 +0300

    ..

--- src/components/BuckConverter.js
+++ src/components/BuckConverter.js
@@ -8,58 +8,101 @@ export default class BuckConverter extends React.Component{
             vout: 5,
             ripple: 0.01,
             freq: 40000,
-            res: 10
+            res: 10,
+            htmlID: {
+                vin:"v-in",
+                vout: "v-out",
+                ripple: "ripple",
+                freq: "freq",
+                res: "res"
+            }
         }
 
+        
+        // this.handleChange = this.handleClick.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(e){
         var regEx = /\d+/gi;
-        console.log(e);
-        if(e.target.id == "v-in"){
-            if(regEx.test(e.target.value)){            
-                this.setState({vin: parseFloat(e.target.value)});
-            }
-            
-        }else if(e.target.id == "v-out"){
+        console.log(e.target);
+        if(e.target.id == this.state.htmlID.vin){
+            // if(regEx.test(e.target.value)){            
+            //     this.setState({vin: parseFloat(e.target.value)});
+            // }
+            this.setState({...this.state, vin: e.target.value});
+        }else if(e.target.id == this.state.htmlID.vout){
+            this.setState({vout: e.target.value});
+        }else if(e.target.id == this.state.htmlID.ripple){
+            this.setState({ripple: e.target.value});
+        }else if(e.target.id == this.state.htmlID.freq){
+            this.setState({freq: e.target.value});
+        }else if(e.target.id == this.state.htmlID.res){
+            this.setState({res: e.target.value});
+        }else{
 
         }
-        console.log(this.state);
+
+        
     }
 
+    // handleClick(e){
+        
+    //     this.setState({
+    //         ...this.state,
+    //         vin: parseFloat(this.state.vin),
+    //         vout: parseFloat(this.state.vout),
+    //         ripple: parseFloat(this.state.ripple),
+    //         freq: parseFloat(this.state.freq),
+    //         res: parseFloat(this.state.res)
+    //     });
+    //     console.log(this.state);
+    // }
+
     showMe(){
-        console.log();
+        this.setState({
+            ...this.state,
+            vin: parseFloat(this.state.vin),
+            vout: parseFloat(this.state.vout),
+            ripple: parseFloat(this.state.ripple),
+            freq: parseFloat(this.state.freq),
+            res: parseFloat(this.state.res)
+        });
+       
+        console.log(this.state);
     }
 
     componentWillUnmount(){
         console.log("unmount");  
     }
 
+    componentDidUpdate(){
+        console.log("updated");
+    }
 
 
     render(){
         return(
             <div>
                 <p>
-                    <label htmlFor="v-in">Vin</label>
-                    <input id="v-in" onChange={this.handleChange} value={this.state.vin}/>
+                    <label htmlFor={this.state.htmlID.vin}>Vin</label>
+                    <input id={this.state.htmlID.vin} onChange={this.handleChange} value={this.state.vin}/>
                 </p>
                 <p>
-                    <label htmlFor="v-out">Vout</label>
-                    <input id="v-out" onChange={this.handleChange} value={this.state.vout}/>
+                    <label htmlFor={this.state.htmlID.vout}>Vout</label>
+                    <input id={this.state.htmlID.vout} onChange={this.handleChange} value={this.state.vout}/>
                 </p>
                 <p>
-                    <label htmlFor="ripple">Ripple</label>
-                    <input id="ripple" onChange={this.handleChange} value={this.state.ripple}/>
+                    <label htmlFor={this.state.htmlID.ripple}>Ripple</label>
+                    <input id={this.state.htmlID.ripple} onChange={this.handleChange} value={this.state.ripple}/>
                 </p>
                 <p>
-                    <label htmlFor="freq">Freq</label>
-                    <input id="freq" onChange={this.handleChange} value={this.state.freq}/>
+                    <label htmlFor={this.state.htmlID.freq}>Freq</label>
+                    <input id={this.state.htmlID.freq} onChange={this.handleChange} value={this.state.freq}/>
                 </p>
                 <p>
-                    <label htmlFor="res">Res</label>
-                    <input id="res" onChange={this.handleChange} value={this.state.res}/>
+                    <label htmlFor={this.state.htmlID.res}>Res</label>
+                    <input id={this.state.htmlID.res} onChange={this.handleChange} value={this.state.res}/>
                 </p>
                 
                 <button onClick={()=>{this.showMe()}}>Calculate</button>
